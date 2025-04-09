978d1ab00c4ea33a0a04d18e8612b6b63a4250b1
mcarroll5389 <103894618+mcarroll5389@users.noreply.github.com>
2025-04-09 14:57:18 +0100

    Update AD_ImportADGroups_v0.1.ps1

--- AD_Import_Export/2-Groups/AD_ImportADGroups_v0.1.ps1
+++ AD_Import_Export/2-Groups/AD_ImportADGroups_v0.1.ps1
@@ -17,7 +17,7 @@ Read-Host "Press Enter to continue"
 $import = Read-Host "Please enter the path to the CSV file you want to import (e.g., C:\path\to\your\file.csv)"
 $import = $import -replace '"',''
 $new_server_fqdn = Read-Host "Please enter the FQDN of the new server (such as server.domain.local)"
-Read-Host "Are you sure you want to import the groups from $import to $new_server_fqdn? Press Enter to continue or Ctrl+C to cancel."
+Read-Host "Are you sure you want to import the groups from $import to $new_server_fqdn ? Press Enter to continue or Ctrl+C to cancel."
 
 $all_groups = Import-Csv -Path $import -Encoding UTF8
 
@@ -39,7 +39,7 @@ foreach ($group in $all_groups)
     if ($?) {
         Write-Host "Group $DisplayName created successfully."
     } else {
-        Write-Host "Failed to create group $DisplayName. It may already exist or there was an error."
+        Write-Host "Failed to create group $DisplayName . It may already exist or there was an error."
     }
 }
 
